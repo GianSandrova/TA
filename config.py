7249783bcb6671b764ed85f1363330ef14252b60
GianSandrova <giansandrova16@gmail.com>
2025-05-04 01:33:30 +0700

    ganti dimensi embedding

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ AUTH = ("neo4j", "12345678")
 
 # Konfigurasi index embedding
 INDEX_NAME = "ayat_embeddings"  # Nama indeks di Neo4j
-DIMENSION = 3584  # Disesuaikan dengan model embedding yang digunakan
+DIMENSION = 4096  # Disesuaikan dengan model embedding yang digunakan
 LABEL = "Tafsir"  # Label node di Neo4j
 EMBEDDING_PROPERTY = "embedding"  # Properti yang menyimpan embedding
 
